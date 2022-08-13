12b0c5bc9c36fc715f4798e31571e211385b8132
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-08-13 15:25:13 -0700

    added options to the db connection

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
-import dotenv from "dotenv"
-dotenv.config()
-import express, { Request, Response } from "express"
-import authRouter from "./routes/authRouter.js"
-import cookieParser from "cookie-parser"
-import mongoose from "mongoose"
+import dotenv from "dotenv";
+dotenv.config();
+import express, { Request, Response } from "express";
+import authRouter from "./routes/authRouter.js";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
 try {
-    mongoose.connect("mongodb://localhost:27017/mono")
-    console.log("Connected")
+  mongoose.connect("mongodb://localhost:27017/mono", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Connected");
 } catch (err) {
-    console.log(err)
-    process.exit(1)
+  console.log(err);
+  process.exit(1);
 }
 
+app.get("/", (req: Request, res: Response) =>
+  res.send("This is the begining of the mono backend")
+);
+app.use("/auth", authRouter);
 
-app.get("/", (req: Request, res:Response) => res.send("This is the begining of the mono backend"))
-app.use("/auth", authRouter)
-
-app.listen(3001, () => console.log("Now listening"))
+app.listen(3001, () => console.log("Now listening"));
